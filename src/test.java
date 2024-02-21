b5b81adf924ba6688e6a849eb9c6f544f12e7d8f
Lea <113557993+leagrabowski@users.noreply.github.com>
2024-02-20 21:02:08 -0600

    test??????????????

---
+++ src/test.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author leagr
+ */
+public class test {
+    
+    int x = 929823847;
+    
+}
